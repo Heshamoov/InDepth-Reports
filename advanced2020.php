b48663d2e10ac8f76f23fe3432fc2797c84c070a
Hesham <heshamoov90@gmail.com>
2021-01-26 12:04:52 +0400

    'Release'

--- advanced2020.php
+++ advanced2020.php
@@ -38,8 +38,8 @@ if (!isset($_SESSION['login'])) {
                     <div class="w3-dropdown-hover">
                         <button class="w3-button w3-green">Benchmark</button>
                         <div class="w3-dropdown-content w3-bar-block w3-border">
-                            <a href="advanced.php" class="w3-bar-item w3-button w3-hover-green">Attainment</a>
-                            <a href="advanced2020.php" class="w3-bar-item w3-button w3-hover-green">2020 Attainment</a>
+<!--                            <a href="advanced.php" class="w3-bar-item w3-button w3-hover-green">Attainment</a>-->
+                            <a href="advanced2020.php" class="w3-bar-item w3-button w3-hover-green">Attainment</a>
                             <a href="cycle.php" class="w3-bar-item w3-button w3-hover-green">Cycle</a>
                             <a href="grades.php" class="w3-bar-item w3-button w3-hover-green">Grades</a>
                         </div>
